854e6b51434b715e5449751fa6e68c85f4363df4
Djohn-12 <djjohnson1101@gmail.com>
2024-03-18 19:37:27 -0500

    bonus tasks

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,12 +1,25 @@
+import styling from "./styling.css"
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
+     {sat.map((data) => (
+          <tr key={data.id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational ? 'Active' : 'Inactive'}</td>
+          </tr>
+        ))}
      <tr>
        <td>Row Data TBD</td>
      </tr>
